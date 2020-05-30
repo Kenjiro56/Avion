ffcf8766fc8b165424c7666af7c9b9ae8d3cb142
HiroseKenjiro <hiroken506@gmail.com>
2020-05-30 15:17:15 +0900

    ステージの改良,aircraftの操作の追加

--- Assets/Scripts/aircraftcontroller.cs
+++ Assets/Scripts/aircraftcontroller.cs
@@ -4,29 +4,47 @@ using UnityEngine;
 
 public class aircraftcontroller : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float speed = 10.0f;
+    
+    void Start(){
+       
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        Vector3 ang = this.transform.localEulerAngles;
+        ang.x = 0;
+        this.transform.Translate(Vector3.right * Time.deltaTime * -speed);
+        float angle = 0.5f;
         if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += new Vector3(0.0f,0.1f,0.0f);
+            //transform.position += new Vector3(0.0f, 0.1f, 0.0f);
+            transform.Rotate(transform.forward, angle);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += new Vector3(0.0f, -0.1f, 0.0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += new Vector3(0.0f, 0.1f, 0.0f);
+            //transform.position += new Vector3(0.0f, -0.1f, 0.0f);
+            transform.Rotate(transform.forward, -angle);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(0.0f, 0.1f, 0.0f);
+            transform.Rotate(transform.up, angle);
+            transform.Rotate(transform.right, -angle);
         }
+        if (Input.GetKeyUp(KeyCode.RightArrow)) {
+            this.transform.localEulerAngles = ang;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(transform.up,-angle);
+            transform.Rotate(transform.right, angle);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            this.transform.localEulerAngles = ang;
+        }
+
     }
 }
